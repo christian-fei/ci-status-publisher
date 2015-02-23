ae1a709fe20dbc556a96924ce6d20afefba729b7
Christian Fei <crifei93@gmail.com>
2015-02-23 08:09:57 +0100

    success test

--- test/fail.js
+++
@@ -1,8 +0,0 @@
-var expect = require('chai').expect
-
-describe('always fails', function () {
-  it('fails miserably', function (done) {
-    expect( 42 ).to.equal( false )    
-    done()
-  });
-})
---
+++ test/success.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect
+
+describe('always passes', function () {
+  it('passes succesfully', function () {
+    expect( 42 ).to.equal( true )
+  });
+})
